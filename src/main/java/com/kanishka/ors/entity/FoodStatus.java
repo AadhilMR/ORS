9c2384af356e074b03af5530ae4d170c182e960e
chamith <chamithvitharana998@gmail.com>
2024-08-26 11:02:53 +0530

    Update the Enum

--- src/main/java/com/kanishka/ors/entity/FoodStatus.java
+++ src/main/java/com/kanishka/ors/entity/FoodStatus.java
@@ -2,4 +2,16 @@ package com.kanishka.ors.entity;
 
 public enum FoodStatus {
     AVAILABLE, NOT_AVAILABLE;
+
+    public static FoodStatus valueOf(int id) throws IllegalArgumentException {
+        if(id<1 || id>2) {
+            throw new IllegalArgumentException("Illegal argument has been passed: " + id);
+        }
+
+        if(id==1) {
+            return AVAILABLE;
+        } else {
+            return NOT_AVAILABLE;
+        }
+    }
 }
