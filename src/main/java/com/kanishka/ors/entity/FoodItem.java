3e78b3c216a45d7516bdc9698a048fa2d7cfaba1
chamith <chamithvitharana998@gmail.com>
2024-08-26 10:10:42 +0530

    Add entities related to food item

---
+++ src/main/java/com/kanishka/ors/entity/FoodItem.java
@@ -0,0 +1,68 @@
+package com.kanishka.ors.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+
+@Entity
+public class FoodItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Double price;
+    @ManyToOne
+    @JoinColumn(name = "imageId", nullable = false)
+    private Image image;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "statusId", nullable = false)
+    private FoodStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public FoodStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FoodStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/kanishka/ors/entity/FoodStatus.java
@@ -0,0 +1,5 @@
+package com.kanishka.ors.entity;
+
+public enum FoodStatus {
+    AVAILABLE, NOT_AVAILABLE;
+}
