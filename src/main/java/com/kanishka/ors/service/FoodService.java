012a30d1f540aee561f7eccb41a4f5bb1c9d0c8d
chamith <chamithvitharana998@gmail.com>
2024-08-26 11:05:49 +0530

    Update food repository

--- src/main/java/com/kanishka/ors/service/FoodService.java
+++ src/main/java/com/kanishka/ors/service/FoodService.java
@@ -2,9 +2,11 @@ package com.kanishka.ors.service;
 
 import java.util.List;
 
+import com.kanishka.ors.dto.FoodDTO;
 import com.kanishka.ors.entity.FoodItem;
 
 public interface FoodService {
     List<FoodItem> getAllFoodItems();
     FoodItem getFoodItem(long id);
+    boolean addFoodItem(FoodDTO foodDTO);
 }
--- src/main/java/com/kanishka/ors/service/FoodServiceImpl.java
+++ src/main/java/com/kanishka/ors/service/FoodServiceImpl.java
@@ -6,16 +6,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kanishka.ors.dto.FoodDTO;
 import com.kanishka.ors.entity.FoodItem;
+import com.kanishka.ors.entity.FoodStatus;
+import com.kanishka.ors.entity.Image;
 import com.kanishka.ors.repo.FoodRepository;
+import com.kanishka.ors.repo.ImageRepository;
 
 @Service
 public class FoodServiceImpl implements FoodService {
     @Autowired
     private final FoodRepository foodRepository;
+    @Autowired
+    private final ImageRepository imageRepository;
 
-    public FoodServiceImpl(FoodRepository foodRepository) {
+    public FoodServiceImpl(FoodRepository foodRepository, ImageRepository imageRepository) {
         this.foodRepository = foodRepository;
+        this.imageRepository = imageRepository;
     }
 
     @Override
@@ -32,5 +39,31 @@ public class FoodServiceImpl implements FoodService {
     @Override
     public List<FoodItem> getAllFoodItems() {
         return foodRepository.findAll();
+    }
+
+    @Override
+    public boolean addFoodItem(FoodDTO foodDTO) {
+        try {
+            FoodStatus status = FoodStatus.valueOf(foodDTO.getAvailability());
+            Image image = addFoodItemImage(foodDTO.getImagePath());
+
+            FoodItem foodItem = new FoodItem();
+            foodItem.setName(foodDTO.getName());
+            foodItem.setPrice(foodDTO.getPrice());
+            foodItem.setImage(image);
+            foodItem.setStatus(status);
+
+            return foodRepository.save(foodItem) != null;
+        } catch (IllegalArgumentException ignored) {
+
+        }
+        return false;
+    }
+
+    private Image addFoodItemImage(String path) {
+        Image image = new Image();
+        image.setPath(path);
+
+        return imageRepository.save(image);
     }
 }
